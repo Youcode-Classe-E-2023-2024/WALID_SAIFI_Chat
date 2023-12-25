a762448a074f23caa3c618ba0ceb58c961a6d2ce
Walid Saifi <saifiwalid21@gmail.com>
2023-12-25 11:43:19 +0100

    Update room_chat_views.php

--- views/room_chat_views.php
+++ views/room_chat_views.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="chat.png" type="image/png">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="style.css">
-  <title>Chat Rooms</title>
-</head>
+
 <body>
 
   <!-- Navbar -->
