37d262911be65db6db3d2ef63d543aa81e10526c
Walid Saifi <saifiwalid21@gmail.com>
2023-12-25 22:57:27 +0100

    Update register_controller.php

--- controllers/register_controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_POST['submit'])) {
-    $username = $_POST['username']; 
-    $password = $_POST['password'];
-    $email = $_POST['email'];
-    $user = new Utilisateur($username, $password, $email);
-    $user->registre();
-}
-?>
