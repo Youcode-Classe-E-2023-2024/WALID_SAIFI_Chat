a5506728ca8ded0b35304f1c111d9e9ac5347427
Walid Saifi <saifiwalid21@gmail.com>
2023-12-25 12:30:59 +0100

    Update _layout.php

--- views/_layout.php
+++ views/_layout.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="chat.png" type="image/png">
+    <link rel="icon" href="../assets/pictures/chat.png" type="image/png">
     <link rel="stylesheet" href="<?= PATH ?>assets/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title><?= ucfirst($page) ?></title>
 
 </head>
 <body>
-    <h1><?= ucfirst($page) ?> View</h1>
+    <!--<h1><?= ucfirst($page) ?> View</h1>-->
 
     <main>
         <?php include_once 'views/' . $page . '_views.php'; ?>
