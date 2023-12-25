01d1ba3545bd3d819f0391b4645a18ea22948bfb
Walid Saifi <saifiwalid21@gmail.com>
2023-12-25 10:00:06 +0100

    Update database.sql

--- _config/database.sql
+++ _config/database.sql
@@ -1,38 +1,37 @@
-CREATE TABLE User (
+CREATE TABLE utilisateur (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
+  utilisateurname VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Friend (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  user_id INT,
+  utilisateur_id INT,
   friend_id INT,
   status VARCHAR(20) NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES User(id),
-  FOREIGN KEY (friend_id) REFERENCES User(id)
+  FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id),
+  FOREIGN KEY (friend_id) REFERENCES utilisateur(id)
 );
 
 CREATE TABLE Room (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
-  type VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE UserRoom (
-  user_id INT,
+CREATE TABLE utilisateurRoom (
+  utilisateur_id INT,
   room_id INT,
-  FOREIGN KEY (user_id) REFERENCES User(id),
+  FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id),
   FOREIGN KEY (room_id) REFERENCES Room(id)
 );
 
 CREATE TABLE Message (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  user_id INT,
+  utilisateur_id INT,
   room_id INT,
   content TEXT,
   timestamp DATETIME,
-  FOREIGN KEY (user_id) REFERENCES User(id),
+  FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id),
   FOREIGN KEY (room_id) REFERENCES Room(id)
 );
