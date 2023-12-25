a8ad283dbb1bc7ae116c749c6ce3d32933cc2271
Walid Saifi <saifiwalid21@gmail.com>
2023-12-25 15:03:53 +0100

    Update register_views.php

--- views/register_views.php
+++ views/register_views.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="../assets/pictures/chat.png" type="image/png">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="style.css">
-  <title>Chat Rooms - Register</title>
-</head>
-
-<body>
 
   <!-- Common Navbar -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#">
-      <img src="../assets/pictures/chat.png" alt="Logo" style="height: 30px; width: auto;">
+      <img src="./assets/pictures/chat.png" alt="Logo" style="height: 30px; width: auto;">
       Chat Rooms
     </a>
   </nav>
@@ -27,28 +14,25 @@
         <div class="card">
           <div class="card-body">
             <h2 class="card-title text-center mb-4">Register</h2>
-            <form id="registerForm">
+            
+            <form id="registerForm" action="index.php?page=register" method="post">
               <div class="form-group">
                 <label for="registerEmail">Email</label>
-                <input type="email" class="form-control" id="registerEmail" placeholder="Enter your email" required>
+                <input type="email" name="email"  class="form-control" id="registerEmail" placeholder="Enter your email" required>
               </div>
               <div class="form-group">
                 <label for="registerEmail">Username</label>
-                <input type="text" class="form-control" id="registerEmail" placeholder="Enter your email" required>
+                <input type="text" name = "usename" class="form-control" id="registerEmail" placeholder="Enter username" required>
               </div>
               <div class="form-group">
                 <label for="registerPassword">Password</label>
-                <input type="password" class="form-control" id="registerPassword" placeholder="Enter your password"
+                <input type="password" name = "password" class="form-control" id="registerPassword" placeholder="Enter password"
                   required>
               </div>
-              <div class="form-group">
-                <label for="confirmPassword">Confirm Password</label>
-                <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm your password"
-                  required>
-              </div>
+              
               <button type="submit" class="btn btn-primary btn-block">Register</button>
             </form>
-            <p class="mt-3 text-center">Already have an account? <a href="login.html">Login</a></p>
+            <p class="mt-3 text-center">Already have an account? <a href="index.php?page=login">Login</a></p>
           </div>
         </div>
       </div>
@@ -80,6 +64,3 @@
       });
     });
   </script>
-</body>
-
-</html>
