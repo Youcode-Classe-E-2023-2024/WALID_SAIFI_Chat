b08877d62cd25a915a3ac6b32dc5ce62d64880df
Walid Saifi <saifiwalid21@gmail.com>
2023-12-25 15:04:05 +0100

    Update _layout.php

--- views/_layout.php
+++ views/_layout.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../assets/pictures/chat.png" type="image/png">
+    <link rel="icon" href="./assets/pictures/chat.png" type="image/png">
     <link rel="stylesheet" href="<?= PATH ?>assets/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title><?= ucfirst($page) ?></title>
