98eb4c2a382049a68730e6c4d612cbad1862fccb
Walid Saifi <saifiwalid21@gmail.com>
2023-12-25 15:09:13 +0100

    Update register_model.php

--- models/register_model.php
+++ models/register_model.php
@@ -1,11 +1,16 @@
 <?php
+echo "hello registre model";
+
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       $usename = $_POST['usename'];
       $password = $_POST['password'];
       $email = $_POST['email'];
-      header("ddd.php");
-  
+     $user = new Utilisateur($usename,$password,$email);
+     $user->registre();
+     header("index.php?page=login");
 }
+
+
 ?>
