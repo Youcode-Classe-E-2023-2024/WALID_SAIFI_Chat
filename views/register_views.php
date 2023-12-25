e6f785f84d756d142beefa78469d0d10a1ead0cd
Walid Saifi <saifiwalid21@gmail.com>
2023-12-25 16:38:35 +0100

    Update register_views.php

--- views/register_views.php
+++ views/register_views.php
@@ -1,68 +1,65 @@
+<!-- Common Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">
+    <img src="./assets/pictures/chat.png" alt="Logo" style="height: 30px; width: auto;">
+    Chat Rooms
+  </a>
+</nav>
 
+<!-- Registration Page -->
+<div class="container mt-4">
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h2 class="card-title text-center mb-4">Register</h2>
 
-  <!-- Common Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">
-      <img src="./assets/pictures/chat.png" alt="Logo" style="height: 30px; width: auto;">
-      Chat Rooms
-    </a>
-  </nav>
-  
+          <form id="registerForm" action="" method="post">
+            <div class="form-group">
+              <label for="registerEmail">Email</label>
+              <input type="email" name="email" class="form-control" id="registerEmail" placeholder="Enter your email" required>
+            </div>
+            <div class="form-group">
+              <label for="registerUsername">Username</label>
+              <input type="text" name="username" class="form-control" id="registerUsername" placeholder="Enter username" required>
+            </div>
+            <div class="form-group">
+              <label for="registerPassword">Password</label>
+              <input type="password" name="password" class="form-control" id="registerPassword" placeholder="Enter password" required>
+            </div>
 
-  <!-- Registration Page -->
-  <div class="container mt-4">
-    <div class="row justify-content-center">
-      <div class="col-md-6">
-        <div class="card">
-          <div class="card-body">
-            <h2 class="card-title text-center mb-4">Register</h2>
+            <button type="submit" name="submit" class="btn btn-primary btn-block">Register</button>
+          </form>
 
-            <form id="registerForm" action="index.php?page=regi" method="post">
-              <div class="form-group">
-                <label for="registerEmail">Email</label>
-                <input type="email" name="email"  class="form-control" id="registerEmail" placeholder="Enter your email" required>
-              </div>
-              <div class="form-group">
-                <label for="registerEmail">Username</label>
-                <input type="text" name = "usename" class="form-control" id="registerEmail" placeholder="Enter username" required>
-              </div>
-              <div class="form-group">
-                <label for="registerPassword">Password</label>
-                <input type="password" name = "password" class="form-control" id="registerPassword" placeholder="Enter password"
-                  required>
-              </div>
-              
-              <button type="submit" class="btn btn-primary btn-block">Register</button>
-            </form>
-            <p class="mt-3 text-center">Already have an account? <a href="index.php?page=login">Login</a></p>
-          </div>
+          <p class="mt-3 text-center">Already have an account? <a href="index.php?page=login">Login</a></p>
         </div>
       </div>
     </div>
   </div>
+</div>
 
-  <!-- Common Scripts JS -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <script>
-    // JavaScript code for registration logic
-    $(document).ready(function () {
-      $('#registerForm').submit(function (event) {
-        event.preventDefault();
-        // Implement your registration logic here
-        // Example: Validate inputs and register user using AJAX
-        // $.ajax({
-        //   type: 'POST',
-        //   url: 'register.php',
-        //   data: $(this).serialize(),
-        //   success: function(response) {
-        //     // Handle success response
-        //   },
-        //   error: function(error) {
-        //     // Handle error response
-        //   }
-        // });
-      });
+<!-- Common Scripts JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script>
+  // JavaScript code for registration logic
+  $(document).ready(function () {
+    $('#registerForm').submit(function (event) {
+      event.preventDefault();
+      // Implement your registration logic here
+      // Example: Validate inputs and register user using AJAX
+      // $.ajax({
+      //   type: 'POST',
+      //   url: 'register.php',
+      //   data: $(this).serialize(),
+      //   success: function(response) {
+      //     // Handle success response
+      //   },
+      //   error: function(error) {
+      //     // Handle error response
+      //   }
+      // });
     });
-  </script>
+  });
+</script>
