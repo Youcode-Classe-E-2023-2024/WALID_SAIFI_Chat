7238e9a17624a23f322cdf0213ddc214724e3f5f
Walid Saifi <saifiwalid21@gmail.com>
2023-12-25 14:25:32 +0100

    Update database.sql

--- _config/database.sql
+++ _config/database.sql
@@ -3,7 +3,7 @@ CREATE TABLE utilisateur (
   utilisateurname VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL
-);
+); ""
 
 CREATE TABLE Friend (
   id INT PRIMARY KEY AUTO_INCREMENT,
