ebf01f77312f0e745aa63a502ac16b78052cfbc6
Walid Saifi <saifiwalid21@gmail.com>
2023-12-25 22:57:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,15 +13,13 @@ spl_autoload_register(function ($class) {
 if (isset($_GET['page']) && !empty($_GET['page'])) {
     $page = trim(strtolower($_GET['page']));
 } else {
-    $page = 'home';
+    $page = 'login';
 }
 $all_pages = scandir('controllers');
 
 
 if (in_array($page . '_controller.php', $all_pages)) {
     include_once 'models/' . $page . '_model.php';
-
-
     include_once 'controllers/' . $page . '_controller.php';
     include_once 'views/_layout.php';
 } else {
