67bf2b5e3391a38785573a4e81c1bcebad3ff327
Walid Saifi <saifiwalid21@gmail.com>
2023-12-25 22:57:09 +0100

    Update register_views.php

--- views/register_views.php
+++ views/register_views.php
@@ -14,7 +14,7 @@
         <div class="card-body">
           <h2 class="card-title text-center mb-4">Register</h2>
 
-          <form id="registerForm" action="index?page=register " method="post">
+          <form id="registerForm" action="" method="post">
             <div class="form-group">
               <label for="registerEmail">Email</label>
               <input type="email" name="email" class="form-control" id="registerEmail" placeholder="Enter your email" required>
@@ -38,7 +38,4 @@
   </div>
 </div>
 
-<!-- Common Scripts JS -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
