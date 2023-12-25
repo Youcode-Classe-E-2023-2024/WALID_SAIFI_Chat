b3938a110f95dc2b2c617fd6261bb367bf03a261
Walid Saifi <saifiwalid21@gmail.com>
2023-12-25 14:25:26 +0100

    Create Database.php

---
+++ _classes/Database.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class Database
+{
+
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $db_name = "Chat";
+    protected $conn;
+
+    public function __construct()
+    {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+    }
+
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+
+
+
+}
+
+
