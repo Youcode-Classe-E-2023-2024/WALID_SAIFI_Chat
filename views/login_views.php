2c9f6a5b03689b16df830993cafb83465ca8031b
Walid Saifi <saifiwalid21@gmail.com>
2023-12-26 09:43:59 +0100

    Update login_views.php

--- views/login_views.php
+++ views/login_views.php
@@ -15,7 +15,7 @@
         <div class="card">
           <div class="card-body">
             <h2 class="card-title text-center mb-4">Login</h2>
-            <form id="loginForm">
+            <form id="loginForm" action="index.php?page=login" method="post">
               <div class="form-group">
                 <label for="email">Email</label>
                 <input type="email" class="form-control" id="email" placeholder="Enter your email" required>
@@ -33,31 +33,7 @@
     </div>
   </div>
 
-  <!-- Common Scripts JS -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <script>
-    // JavaScript code for login logic
-    $(document).ready(function () {
-      $('#loginForm').submit(function (event) {
-        event.preventDefault();
-        // Implement your login logic here
-        // Example: Validate credentials using AJAX
-        // $.ajax({
-        //   type: 'POST',
-        //   url: 'login.php',
-        //   data: $(this).serialize(),
-        //   success: function(response) {
-        //     // Handle success response
-        //   },
-        //   error: function(error) {
-        //     // Handle error response
-        //   }
-        // });
-      });
-    });
-  </script>
+
 </body>
 
 </html>
