bcdc7a9f35225ef7e8f99c5b2c4dfed9e78f30d8
Walid Saifi <saifiwalid21@gmail.com>
2023-12-26 12:29:44 +0100

    Update login_controller.php

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -3,11 +3,8 @@
 if (isset($_POST['submit'])) {
     $password = $_POST['password'];
     $email = $_POST['email'];
-
     if (Utilisateur::login($password, $email)) {
-
         session_start();
-        
         $_SESSION['email'] = $email;
         $_SESSION['id'] = Utilisateur::getId($email);
 
