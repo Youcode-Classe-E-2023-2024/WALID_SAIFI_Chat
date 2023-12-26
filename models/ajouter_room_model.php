ebfcbe2f8dcebe067a24f302d14b5d478d6ba4e0
Walid Saifi <saifiwalid21@gmail.com>
2023-12-26 14:24:41 +0100

    Update ajouter_room_model.php

--- models/ajouter_room_model.php
+++ models/ajouter_room_model.php
@@ -1,7 +1,11 @@
 <?php
 
 if($_POST['submit']){
+$name =$_POST['name'];
+$tab_user=$_POST['user_list'];
+$id = Room::createRoom($name);
 
-
-
+     foreach ($tab_user as $tab){
+      Room::user_Room($tab,$id);
+     }
 }
