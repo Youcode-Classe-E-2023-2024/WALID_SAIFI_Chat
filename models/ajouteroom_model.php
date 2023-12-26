b17ddd78bf58cc4dd391ffb0d32666077ba3a739
Walid Saifi <saifiwalid21@gmail.com>
2023-12-26 15:23:58 +0100

    Update ajouteroom_model.php

--- models/ajouteroom_model.php
+++ models/ajouteroom_model.php
@@ -1,12 +1,14 @@
 <?php
 
-if(isset($_POST['submit'])){
-     $name =$_POST['name'];
-      $tab_user=$_POST['user_list'];
-      if(isset($name) && isset($tab_user)) {
-          $id = Room::createRoom($name);
-          foreach ($tab_user as $tab) {
-              Room::user_Room($tab, $id);
+if(isset($_POST['submit']) ){
+      if(isset($_POST['name']) && isset($_POST['user_list'])) {
+          $name = $_POST['name'];
+          $tab_user = $_POST['user_list'];
+          if (isset($name) && isset($tab_user)) {
+              $id = Room::createRoom($name);
+              foreach ($tab_user as $tab) {
+                  Room::user_Room($tab, $id);
+              }
           }
       }
 
