302bd8fdc7f5af29f61b21bb2942045e6c24fe43
Walid Saifi <saifiwalid21@gmail.com>
2023-12-27 15:00:39 +0100

    Update Room.php

--- _classes/Room.php
+++ _classes/Room.php
@@ -12,20 +12,21 @@ class Room
         $this->id = $room['id'];
         $this->name = $room['room'];
     }
-    static  public function createRoom($name){
+    static public function createRoom($name, $id) {
         global $db;
-        $sql = "INSERT INTO room (name) VALUES (?)";
-
+        $sql = "INSERT INTO room (name, id_createur) VALUES (?, ?)";
+    
         $insert = $db->prepare($sql);
-
-        $insert->bind_param("s",$name);
-
+        
+        $insert->bind_param("ss", $name, $id);
+    
         if ($insert->execute()) {
             return $db->insert_id;
         } else {
             return false;
         }
     }
+    
     static public function user_Room($id_user, $id_room){
         global $db;
 
@@ -36,12 +37,25 @@ class Room
         $insert->execute();
 
     }
-    static public function getAllroom(){
+    static public function getAllroom($id){
         global $db;
-        $result = $db->query("SELECT * FROM  room");
+        $result = $db->query("SELECT * FROM room JOIN utilisateur_room ON room_id=id WHERE utilisateur_id='$id' ORDER by room.id DESC;");
         return $result->fetch_all(MYSQLI_ASSOC);
 
     }
+    static public function getAll_dernier_room($id){
+        global $db;
+        $result = $db->query("SELECT * FROM room WHERE id IN
+         (SELECT MAX(id) FROM room JOIN utilisateur_room ON room.id = utilisateur_room.room_id WHERE utilisateur_room.utilisateur_id = '$id')");
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+    static public function get_All_massage_room($id){
+        global $db;
+        $result = $db->query("SELECT * FROM message WHERE utilisateur_id = $id");
+        return $result->fetch_all(MYSQLI_ASSOC);
+
+    }
+    
 
 
 }
