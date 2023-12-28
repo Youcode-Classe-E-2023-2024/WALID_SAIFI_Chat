eb7b5f21c243d094a4b71d6bfb3d408b782b578f
Walid Saifi <saifiwalid21@gmail.com>
2023-12-29 00:45:19 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,10 @@
-
 document.getElementById('sendButton').addEventListener('click', function() {
     var userId = document.getElementById('userIdInput').value;
     var roomId = document.getElementById('roomIdInput').value;
     var message = document.getElementById('messageInput').value;
   
     $.ajax({
-        url: 'index.php?page=room', 
+        url: 'index.php?page=room', // Update with the actual path
         type: 'POST',
         data: {
             user_id: userId,
