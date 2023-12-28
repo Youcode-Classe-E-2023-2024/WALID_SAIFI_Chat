9a6c0fd9e0bb35ef82d8326000dcdfb7035ee27d
Walid Saifi <saifiwalid21@gmail.com>
2023-12-28 14:42:25 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
-function message(){
-    
+function sendMessage() {
+    var xhr = new XMLHttpRequest();
+    var message = document.getElementById("message").value;
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("response").innerHTML = this.responseText;
+        }
+    };
+    xhr.open("GET", "index.php?page=room&message=" + encodeURIComponent(message), true);
+    xhr.send();
 }
