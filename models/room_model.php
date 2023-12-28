3576ee5aa5f7958f231b0e2949ceeba16dae72a8
Walid Saifi <saifiwalid21@gmail.com>
2023-12-29 00:45:15 +0100

    Update room_model.php

--- models/room_model.php
+++ models/room_model.php
@@ -5,4 +5,10 @@ $_SESSION['id_room']=$id;
 
 $res = Room::get_Allroom_massage($id);
 
-
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['user_id']) && isset($_POST['room_id']) && isset($_POST['messageInput'])){
+    $userId = $_POST['user_id'];
+    $roomId = $_POST['room_id'];
+    $content = $_POST['messageInput'];
+    Room::ajouter_message($userId,$roomId,$content);
+    exit();
+}
