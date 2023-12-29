3ac9e2dc862bd71c42b4f22fdce93ded06f67945
Walid Saifi <saifiwalid21@gmail.com>
2023-12-29 01:10:55 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,28 @@ document.getElementById('sendButton').addEventListener('click', function() {
         }
     });
   });
+
+
+  $(document).ready(function () {
+    // Function to load room messages via AJAX
+    function loadRoomMessages() {
+        var roomId = <? php echo json_encode($_GET['id']); ?>; 
+
+        $.ajax({
+            type: 'GET',
+            url: 'get_room_messages.php',
+            data: {roomId: roomId},
+            success: function (response) {
+                $('#messages-section').html(response);
+            },
+            error: function () {
+                alert('Error loading room messages.');
+            }
+        });
+    }
+
+    loadRoomMessages();
+
+ 
+    setInterval(loadRoomMessages, 5000); 
+});
